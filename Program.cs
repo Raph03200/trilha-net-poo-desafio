dcff5349dbabd5387ec882b0229e6742b632e9cb
Raphael Façanha <raphafacanha@gmail.com>
2024-01-09 16:27:39 -0300

    Desafio de POO Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Telefone Nokia");
+Smartphone n1 = new Nokia(numero:"999999",modelo:"Tijolao",imei:"11111111111",memoria:64);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo(nomeApp:"Spotify");
+
+
+Console.WriteLine("Telefone Iphone");
+Smartphone i1 = new Iphone(numero:"9777777",modelo:"Iphone X",imei:"123456789",memoria:128);
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo(nomeApp: "ITunes");
